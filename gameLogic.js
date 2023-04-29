dbe44797ba2c25b2ef5abd0603dd112a7036fbad
theamzhao <126366310+theamzhao@users.noreply.github.com>
2023-04-29 17:18:43 -0400

    Fixed JS logic for game, changed Pokemon theme

--- gameLogic.js
+++ gameLogic.js
@@ -1,7 +1,28 @@
 console.log("Welcome to the Odin Project - Rock Paper Scissors");
 
+// set buttons to node list of options
+const buttons = document.querySelectorAll('button');
+
+// tells what round player is on
+const roundStatus = document.createElement('h3'); 
+roundStatus.id = 'round';
+container.appendChild(roundStatus);
+
+// tells what the outcome of the round is + current score
+const gameStatus = document.createElement('h3');  
+gameStatus.id = 'game';
+container.appendChild(gameStatus);
+
+// placeholders to know where data will update
+document.getElementById('round').textContent = 'Select a Pokemon to start';
+console.log(roundStatus.textContent);
+
+var playerScore = 0;
+var computerScore = 0;
+var round = 1;
+
 function getComputerChoice() {
-    let choiceList = ['Rock','Paper','Scissors'];
+    let choiceList = ['Grass','Fire','Water'];
     let choice = choiceList[Math.floor(Math.random()*choiceList.length)];
     return choice;
 }
@@ -11,81 +32,37 @@ function playRound(computerSelect, playerSelect) {
     let firstLetter = playerSelect.substring(0,1).toUpperCase();
     playerSelect = firstLetter + playerSelect.substring(1).toLowerCase();
 
-    if (playerSelect == "Rock" && computerSelect == "Scissors") {
-        return `You win! ${playerSelect} beats ${computerSelect}.`;
-    } else if (playerSelect == "Scissors" && computerSelect == "Paper") {
-        return `You win! ${playerSelect} beats ${computerSelect}.`;
-    } else if (playerSelect == "Paper" && computerSelect == "Rock") {
-        return `You win! ${playerSelect} beats ${computerSelect}`;
+    if (playerSelect == "Grass" && computerSelect == "Water") {
+        document.getElementById('round').textContent = `You win! ${playerSelect} beats ${computerSelect}.`;
+    } else if (playerSelect == "Water" && computerSelect == "Fire") {
+        document.getElementById('round').textContent = `You win! ${playerSelect} beats ${computerSelect}.`;
+    } else if (playerSelect == "Fire" && computerSelect == "Grass") {
+        document.getElementById('round').textContent =  `You win! ${playerSelect} beats ${computerSelect}.`;
     } else if (playerSelect == computerSelect) {
-        return `Oops, you tied. You both selected ${playerSelect}.`;
+        document.getElementById('round').textContent = `Oops, you tied. You both selected ${playerSelect}.`;
     } else {
-        return `You lose. ${computerSelect} beats ${playerSelect}.`;
+        document.getElementById('round').textContent = `You lose. ${computerSelect} beats ${playerSelect}.`;
     }
 }
 
-function playGame(roundStatus, rounds, buttons, buttonid) {
-    var playerScore = 0;
-    var computerScore = 0;
-    
-
-    for (let round = 1; round < 6; round++) {
-        rounds.textContent = `Round ${round}: Rock, Paper or Scissors?`;
-
-        roundStatus.textContent = `${playRound(getComputerChoice(),buttonid)}`;
-  
-        if (roundStatus.textContent.includes('win')) {
-            playerScore++;
-        } else if (roundStatus.textContent.includes('lose')) {
-            computerScore++;
-        } else {
-        }
-    
-        console.log(roundStatus.textContent);
-        let currScore = `your current score is ${playerScore}, and the computer's score is ${computerScore}.`;
-        roundStatus.textContent = `End of round ${round}, ${currScore}`;
-        
-    }
-
-    if (playerScore > computerScore) {
-        let decision = `Congratulations! You won. Your score was ${playerScore} to ${computerScore}`;
-        return decision;
-    } else if (playerScore < computerScore) {
-        let decision = `Too bad, you lost. Your score was ${playerScore} to ${computerScore}`;
-        return decision;
-    } else {
-        let decision = `You tied! Your score was ${playerScore} to ${computerScore}.`;
-        return decision;
-    }
-}
-
-// buttons is a node list. It looks and acts much like an array.
-const buttons = document.querySelectorAll('button');
-
-//tells what round player is on
-const rounds = document.createElement('h2'); 
-rounds.classList.add('rounds');
-container.appendChild(rounds);
-
-//tells what the outcome of the round is
-const roundStatus = document.createElement('h2');  
-roundStatus.classList.add('status');
-container.appendChild(roundStatus);
-
-//tells what the current score is
-const currScore = document.createElement('h2'); 
-roundStatus.classList.add('status');
-container.appendChild(roundStatus);
-
-
-// we use the .forEach method to iterate through each button
+// using .forEach method to iterate through each button
 buttons.forEach((button) => {
-
     button.addEventListener('click', () => {
-        //alert(playRound(getComputerChoice(),button.id));
-        playGame(roundStatus, rounds, buttons, button.id);
-        //message.textContent = `${playRound(getComputerChoice(),button.id)}`;
+        
+        if (playerScore < 5 && computerScore < 5){
+            playRound(getComputerChoice(),button.id);
+            
+            if (roundStatus.textContent.includes('win')){
+                playerScore++;
+            } else if (roundStatus.textContent.includes('lose')){
+                computerScore++;
+            } else if (roundStatus.textContent.includes('Oops')){
+                console.log("ya tied");
+            } else console.log("wut");
+            gameStatus.textContent = (`player score is ${playerScore} and computer score is ${computerScore}`);
+        } else if (playerScore == 5) alert(`Game over, you won!`);
+        else if (computerScore == 5) alert('Game over, you lose!');
+
     });
 });
 
-//console.log(playRound(getComputerChoice(),playerSelect));
